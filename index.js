f82f07aa14eea2ec43b3d19e2c0795a88f5d360e
Marquis520 <Marquis520@icloud.com>
2021-03-21 23:23:30 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// FIRST TEST
+const main = document.getElementById('main');
+main.remove();
+
+//SECOND & THIRD TEST
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+
+//FOURTH TEST
+newHeader.innerHTML = 'Marquis is the champion';
